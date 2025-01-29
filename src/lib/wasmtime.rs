0a67ffbd2a43a5b5b8811910ff836a2ead93e9df
EuenSkreuen <aaalalpe@jyu.fi>
2025-01-29 06:36:34 +0200

    Modules for testing, reworked the wasmtime structure thingy

--- src/lib/wasmtime.rs
+++ src/lib/wasmtime.rs
@@ -2,16 +2,15 @@
 //! This module contains all functionality related to wasmtime
 //! 
 
-
+use std::collections::HashMap;
 use anyhow::Result;
 use wasmtime;
 
 // ----------------------- Wasmtime related functions etc... ----------------------- //
-// TODO: Divide into classes etc like in python source?
+// TODO: Module serialization, also how necessary is it? (details in https://docs.wasmtime.dev/api/wasmtime/struct.Module.html )
 // TODO: Implement missing functionality
-// TODO: Are traits needed? In the original there were superclasses for wasm runtime and
-// wasm modules, likely because they were using both wasm3 and wasmtime. Are both engines
-// needed?
+// TODO: Are both wasmtime and wasm3 needed?
+// TODO: Wtf is going on with life parameters? Doublecheck that they make sense.
 
 /// Testfunction that runs the fibonacci wasm module
 pub fn run_fibonacci() -> Result<i64, Box<dyn std::error::Error>> {
@@ -25,83 +24,16 @@ pub fn run_fibonacci() -> Result<i64, Box<dyn std::error::Error>> {
     Ok(result)
 }
 
-/// Testfunction to run fibonacci module by using the fibo_from_pointers function
-pub fn run_fibonacci2() -> Result<u64, Box<dyn std::error::Error>> {
-    // Initialize engine, module, linker, and store.
-    let engine = wasmtime::Engine::default();
-    let module = wasmtime::Module::from_file(&engine, "tests/fibo.wasm")?;
-    let mut linker = wasmtime::Linker::new(&engine);
-    let mut store = wasmtime::Store::new(&engine, ());
-
-    // Instantiate the WASM module.
-    let instance = linker.instantiate(&mut store, &module)?;
-
-    // Acquire the exported linear memory named "memory".
-    let memory = instance
-        .get_memory(&mut store, "memory")
-        .expect("Failed to find memory in the WASM module");
-
-    // Acquire the `fibo_from_pointers` function.
-    // Note: we map function signature (pointer types) to integer offsets.
-    // So `(i32, i32, i32) -> i32` in Wasmtime.
-    let fibo_from_pointers: wasmtime::TypedFunc<(i32, i32, i32), i32> = instance
-        .get_typed_func(&mut store, "fibo_from_pointers")
-        .expect("Failed to find `fibo_from_pointers` function");
-
-    // The input we want to calculate the Fibonacci for.
-    let input_val = 10u64;
-    let input_bytes = input_val.to_le_bytes();
-
-    // Offsets in the linear memory where we place our data.
-    //  - We'll write the 8 input bytes at offset 0.
-    //  - We'll store 1 byte for output length at offset 8.
-    //  - The WASM function will return a pointer (offset) to the result bytes.
-    let input_ptr = 0;
-    let output_len_ptr = 8;
-
-    // Write our input (8 bytes) to memory at offset 0.
-    memory.write(&mut store, input_ptr, &input_bytes)?;
-
-    // Initialize the output length byte to 0 at offset 8.
-    memory.write(&mut store, output_len_ptr, &[0u8])?;
-
-    // Now call `fibo_from_pointers(offset_of_input, length_of_input, offset_of_output_len)`.
-    // This will return an offset (i32) to where the result bytes begin in WASM memory.
-    let result_ptr = fibo_from_pointers.call(
-        &mut store,
-        (
-            input_ptr as i32,
-            input_bytes.len() as i32,
-            output_len_ptr as i32,
-        ),
-    )?;
-
-    // Read the output length (1 byte) from memory at offset 8.
-    let mut out_len_buf = [0u8; 1];
-    memory.read(&store, output_len_ptr, &mut out_len_buf)?;
-    let output_len = out_len_buf[0] as usize;
-
-    // Now read `output_len` bytes from memory at the `result_ptr` offset.
-    let mut result_bytes = vec![0u8; output_len];
-    memory.read(&store, result_ptr as usize, &mut result_bytes)?;
-
-    // Convert the little-endian bytes to a `u64`.
-    let result = u64::from_le_bytes(
-        result_bytes
-            .try_into()
-            .expect("Expected 8 bytes for the Fibonacci result"),
-    );
-
-    Ok(result)
-}
-
 
 // ----------------------- Wasmtime Runtime related functionality (check python source) ----------------------- //
 
 pub struct WasmtimeRuntime {
-    engine: wasmtime::Engine,
-    store: wasmtime::Store<()>,
-    linker: wasmtime::Linker<()>
+    pub engine: wasmtime::Engine,
+    pub store: wasmtime::Store<()>,
+    pub linker: wasmtime::Linker<()>,
+    pub modules: HashMap<String, WasmtimeModule>,
+    pub functions: Option<HashMap<String, WasmtimeModule>>,
+    pub current_module_name: Option<String> // TODO: Does this have any purpose?
 }
 
 impl WasmtimeRuntime {
@@ -112,31 +44,57 @@ impl WasmtimeRuntime {
         let engine: wasmtime::Engine = wasmtime::Engine::new(&config).unwrap();
         let store: wasmtime::Store<()> = wasmtime::Store::new(&engine, ());
         let linker: wasmtime::Linker<()> = wasmtime::Linker::new(&engine);
+        let modules: HashMap<String, WasmtimeModule> = HashMap::new();
+        let functions = None; // TODO: What exactly should this be?
+        let current_module_name = None;
         Self {
             engine,
             store,
-            linker
+            linker,
+            modules,
+            functions,
+            current_module_name
         }
     }
 
-    /// Loads a module into the wasmtime runtime
-    /// // TODO: Add ModuleConfig instead of some reference bytes?
-    pub fn load_module(&mut self, module_bytes: &[u8]) -> WasmtimeModule{
-        let module = wasmtime::Module::new(&self.engine, module_bytes).unwrap();
-        let instance = self.linker.instantiate(&mut self.store, &module).unwrap();
-        WasmtimeModule { instance }
+
+    // @property
+    // def functions(self) -> Dict[str, WasmModule]:
+    //     """Get the functions loaded in the Wasm runtime and their corresponding modules."""
+    //     if self._functions is None:
+    //         self._functions = {}
+    //         for _, module in self._modules.items():
+    //             for function_name in module.functions:
+    //                 self._functions[function_name] = module
+
+    //     return self._functions
+
+
+    pub fn load_module(&mut self, config: ModuleConfig) -> Result<(), Box<dyn std::error::Error>>{
+        if !self.modules.contains_key(&config.name){
+            let module_name: String = config.name.clone();
+            let module = wasmtime::Module::from_file(&self.engine, config.path.clone())?;
+            let instance = self.linker.instantiate(&mut self.store, &module)?;
+            let mut wasmtime_module = WasmtimeModule::new(config)?;
+            wasmtime_module.module = Some(module);
+            wasmtime_module.instance = Some(instance);
+            self.modules.insert(module_name.clone(), wasmtime_module);
+            println!("Module {} loaded successfully.", module_name);
+        } else {
+            println!("Module {} is already loaded.", &config.name);
+        }
+        Ok(())
     }
 
+
     /// Read from wasmtime runtime memory and return the result
-    pub fn read_from_memory(&self, memory: &wasmtime::Memory, offset: usize, length: usize) -> Vec<u8> {
-        let data = memory.data(&self.store);
-        data[offset..offset + length].to_vec()
+    pub fn read_from_memory() {
+        unimplemented!();
     }
 
     /// Write to wasmtime runtime memory
-    pub fn write_to_memory(&mut self, memory: &wasmtime::Memory, offset: usize, data: &[u8]) {
-        let mem = memory.data_mut(&mut self.store);
-        mem[offset..offset + data.len()].copy_from_slice(data);
+    pub fn write_to_memory() {
+        unimplemented!();
     }
 
     // Link remote functions to wasmtime runtime for use by wasm modules.
@@ -151,28 +109,56 @@ impl WasmtimeRuntime {
 // ----------------------- Wasmtime module related functionality (check python source) ----------------------- //
 
 pub struct WasmtimeModule {
-    instance: wasmtime::Instance
+    pub module: Option<wasmtime::Module>,
+    pub instance: Option<wasmtime::Instance>,
+    pub id: String,
+    pub name: String,
+    pub path: String,
+    pub functions: Option<Vec<String>>
 }
 
 impl WasmtimeModule {
 
+    pub fn new(config: ModuleConfig) -> Result<Self, Box<dyn std::error::Error>> {
+        let module = None;
+        let instance = None; 
+        let functions = None;
+        let wasmtime_module = WasmtimeModule {
+            module: module,
+            instance: instance,
+            id: config.id,
+            name: config.name,
+            path: config.path,
+            functions: functions
+        };
+        Ok(wasmtime_module)
+    }
+
     /// Gets the wasmtime linear memory
-    pub fn get_memory(&self, store: &mut wasmtime::Store<()>, memory_name: &str) -> wasmtime::Memory {
-        self.instance
-            .get_memory(store, memory_name)
-            .expect("Failed to get memory from module")
+    pub fn get_memory() {
+        unimplemented!();
     }
 
     /// Gets a function with given name from current wasm module
-    pub fn get_function(&self, store: &mut wasmtime::Store<()>, func_name: &str) -> wasmtime::Func {
-        self.instance
-            .get_func(store, func_name)
-            .expect("Failed to get function from module")
+    pub fn get_function() {
+        unimplemented!();
     }
 
     /// Gets the names of all known functions in current wasm module
-    pub fn get_all_functions() {
-        unimplemented!();
+    pub fn get_all_functions(&self) -> Vec<String> {
+
+        if let Some(module_reference) = &self.module {
+            let mut funcs: Vec<String> = vec![];
+            let imports = module_reference.imports();
+            for import in imports {
+                if import.ty().func().is_some() {
+                    funcs.push(import.name().to_string());
+                }
+            }
+            return funcs;
+        } else {
+            return vec![];
+        }
     }
 
     /// Gets the argument types of a function in the current wasm module
@@ -181,34 +167,14 @@ impl WasmtimeModule {
     }
 
     /// Run a function in the current wasm module with given parameters and return result
-    pub fn run_function(
-        &self,
-        store: &mut wasmtime::Store<()>,
-        func_name: &str,
-        params: &[wasmtime::Val],
-    ) -> Result<Vec<wasmtime::Val>, wasmtime::Trap> {
-        // 1) Get the `Func` from the module instance
-        let func = self.get_function(store, func_name);
-    
-        // 2) Determine how many results this function returns
-        let ftype = func.ty(store);
-        let results_count = ftype.results().len();
-    
-        // 3) Allocate a results buffer. The values written here will be 
-        // overwritten by Wasmtime, so `Val::I32(0)` is just a placeholder.
-        let mut results_buffer = vec![wasmtime::Val::I32(0); results_count];
-    
-        // 4) Call the function with your parameters and the results buffer
-        func.call(&mut store, params, &mut results_buffer);
-    
-        // 5) Return the results
-        Ok(results_buffer)
-    }
-    
-    
-
-    /// Loads the current module into the current wasm runtime
-    pub fn load_module() {
+    pub fn run_function() {
+        // let engine: wasmtime::Engine = wasmtime::Engine::default();
+        // let module:wasmtime::Module  = wasmtime::Module::from_file(&engine, "tests/fibo.wasm")?;
+        // let linker: wasmtime::Linker<u32> = wasmtime::Linker::new(&engine);
+        // let mut store: wasmtime::Store<u32> = wasmtime::Store::new(&engine, 4);
+        // let instance: wasmtime::Instance = linker.instantiate(&mut store, &module)?;
+        // let fibo: wasmtime::TypedFunc<i64, i64> = instance.get_typed_func::<i64, i64>(&mut store, "fibo")?;
+        // let result: i64 = fibo.call(&mut store, 5)?;
         unimplemented!();
     }
 
@@ -217,5 +183,91 @@ impl WasmtimeModule {
         unimplemented!();
     }
 
+    pub fn functions() {
+        unimplemented!();
+    }
+
+    // @property
+    // def functions(self) -> List[str]:
+    //     """Get the names of the known functions of the Wasm module."""
+    //     if self._functions is None:
+    //         if self.runtime is None:
+    //             self._functions = []
+    //         else:
+    //             self._functions = self._get_all_functions()
+    //     return self._functions
+
+    pub fn run_data_function() {
+        unimplemented!();
+    }
+
+    pub fn upload_data() {
+        unimplemented!();
+    }
+
+    pub fn upload_data_file() {
+        unimplemented!();
+    }
+
+    pub fn upload_ml_model() {
+        unimplemented!();
+    }
+
+    pub fn run_ml_inference() {
+        unimplemented!();
+    }
+
 }
 
+// ----------------------- Miscellaneous module related things ----------------------- //
+
+/// Struct for containing module name, file location and associated files referred to as 'mounts'. 
+/// This is what a module instance for running functions is created based on.
+pub struct ModuleConfig {
+    pub id: String,
+    pub name: String,
+    pub path: String,
+    pub data_files: HashMap<String, String>,
+    pub ml_model: Option<MLModel>,
+    pub data_ptr_function_name: String
+}
+
+impl ModuleConfig {
+    pub fn new(id: String, name: String, path: String, data_files: HashMap<String, String>, ml_model: Option<MLModel>) -> Self {
+        ModuleConfig {
+            id,
+            name,
+            path,
+            data_files,
+            ml_model,
+            data_ptr_function_name: "get_image_ptr".to_string()
+        }
+    }
+
+    pub fn set_model_from_data_files(&mut self, key: Option<String>) {
+        let mut model_name: String = "model.pb".to_string();
+        match key {
+            Some(key_str) => model_name = key_str,
+            None => ()
+        }
+        self.ml_model = Some(MLModel::new(model_name));
+    }
+
+}
+
+/// Struct for ML models
+pub struct MLModel {
+    pub path: String,
+    pub alloc_function_name: String,
+    pub infer_function_name: String
+}
+
+impl MLModel {
+    pub fn new(path: String) -> Self {
+        MLModel {
+            path,
+            alloc_function_name: "alloc".to_string(),
+            infer_function_name: "infer_from_ptrs".to_string()
+        }
+    }
+}
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,22 +1,252 @@
-use supervisor;
+use supervisor::lib::wasmtime::{WasmtimeModule, WasmtimeRuntime, ModuleConfig, MLModel};
+use std::collections::HashMap;
 
 // ----------------------- Wasmtime related tests ----------------------- //
-// TODO: Add test for each existing example .wasm file
+// TODO: Improve the individual tests for each module
 // TODO: Find another way of including modules to tests than having them directly in this repo
 
 #[cfg(test)]
 mod wasmtime_tests {
+    use super::*;
+    const TEST_PATH: &str = "tests/";
 
     #[test]
-    fn fibonacci_test() -> Result<(), Box<dyn std::error::Error>> {
-        let fib_result: i64 = supervisor::lib::wasmtime::run_fibonacci()?;
-        println!("Fibonacci result for 5: {}", fib_result);
-        Ok(())
+    fn test_camera() -> Result<(), Box<dyn std::error::Error>> {
+        // stacktace suppression: https://users.rust-lang.org/t/test-should-panic-somehow-hide-stack-traces/57715
+        let f = |_: &std::panic::PanicHookInfo| {};
+        std::panic::set_hook(Box::new(f));
+        let module_name = "camera.wasm";
+        let data_files: HashMap<String, String> = HashMap::new();
+        let mut runtime = WasmtimeRuntime::new();
+        let module_path = format!("{}{}", TEST_PATH, module_name);
+        let config = ModuleConfig::new(
+            "aaaa-bbbbb-ccccc".to_string(),
+            "camera.wasm".to_string(),
+            module_path.clone(),
+            data_files.clone(),
+            None,
+        );
+        let result = runtime.load_module(config);
+        match result {
+            Ok(_) => {
+                println!("✅ Successfully loaded: {}", module_name);
+            }
+            Err(e) => {
+                println!("❌ Failed to load {}: {}", module_name, e);
+                assert!(false, "");
+            }
+        }
+        if let Some(module) = runtime.modules.get("camera.wasm") {
+            let funcs = module.get_all_functions();
+            println!("Module {} has following function imports: {:?}", module.name, funcs);
+            Ok(())
+        } else {
+            Ok(())
+        }
     }
-    
+
     #[test]
-    fn test_wasmtime_runtime_instance() {
-        let _ = supervisor::lib::wasmtime::WasmtimeRuntime::new();
+    fn test_fibo() -> Result<(), Box<dyn std::error::Error>> {
+        let f = |_: &std::panic::PanicHookInfo| {};
+        std::panic::set_hook(Box::new(f));
+        let module_name = "fibo.wasm";
+        let data_files: HashMap<String, String> = HashMap::new();
+        let mut runtime = WasmtimeRuntime::new();
+        let module_path = format!("{}{}", TEST_PATH, module_name);
+        let config = ModuleConfig::new(
+            "aaaa-bbbbb-ccccc".to_string(),
+            "fibo.wasm".to_string(),
+            module_path.clone(),
+            data_files.clone(),
+            None,
+        );
+        let result = runtime.load_module(config);
+        match result {
+            Ok(_) => {
+                println!("✅ Successfully loaded: {}", module_name);
+            }
+            Err(e) => {
+                println!("❌ Failed to load {}: {}", module_name, e);
+                assert!(false, "");
+            }
+        }
+        if let Some(module) = runtime.modules.get("fibo.wasm") {
+            let funcs = module.get_all_functions();
+            println!("Module {} has following function imports: {:?}", module.name, funcs);
+            Ok(())
+        } else {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_fibobin() -> Result<(), Box<dyn std::error::Error>> {
+        let f = |_: &std::panic::PanicHookInfo| {};
+        std::panic::set_hook(Box::new(f));
+        let module_name = "fibobin.wasm";
+        let data_files: HashMap<String, String> = HashMap::new();
+        let mut runtime = WasmtimeRuntime::new();
+        let module_path = format!("{}{}", TEST_PATH, module_name);
+        let config = ModuleConfig::new(
+            "aaaa-bbbbb-ccccc".to_string(),
+            "fibobin.wasm".to_string(),
+            module_path.clone(),
+            data_files.clone(),
+            None,
+        );
+        let result = runtime.load_module(config);
+        match result {
+            Ok(_) => {
+                println!("✅ Successfully loaded: {}", module_name);
+            }
+            Err(e) => {
+                println!("❌ Failed to load {}: {}", module_name, e);
+                assert!(false, "");
+            }
+        }
+        if let Some(module) = runtime.modules.get("fibobin.wasm") {
+            let funcs = module.get_all_functions();
+            println!("Module {} has following function imports: {:?}", module.name, funcs);
+            Ok(())
+        } else {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_grayscale() -> Result<(), Box<dyn std::error::Error>> {
+        let f = |_: &std::panic::PanicHookInfo| {};
+        std::panic::set_hook(Box::new(f));
+        let module_name = "greyscale.wasm";
+        let data_files: HashMap<String, String> = HashMap::new();
+        let mut runtime = WasmtimeRuntime::new();
+        let module_path = format!("{}{}", TEST_PATH, module_name);
+        let config = ModuleConfig::new(
+            "aaaa-bbbbb-ccccc".to_string(),
+            "greyscale.wasm".to_string(),
+            module_path.clone(),
+            data_files.clone(),
+            None,
+        );
+        let result = runtime.load_module(config);
+        match result {
+            Ok(_) => {
+                println!("✅ Successfully loaded: {}", module_name);
+            }
+            Err(e) => {
+                println!("❌ Failed to load {}: {}", module_name, e);
+                assert!(false, "");
+            }
+        }
+        if let Some(module) = runtime.modules.get("grayscale.wasm") {
+            let funcs = module.get_all_functions();
+            println!("Module {} has following function imports: {:?}", module.name, funcs);
+            Ok(())
+        } else {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_invert_colors() -> Result<(), Box<dyn std::error::Error>> {
+        let f = |_: &std::panic::PanicHookInfo| {};
+        std::panic::set_hook(Box::new(f));
+        let module_name = "invert_colors.wasm";
+        let data_files: HashMap<String, String> = HashMap::new();
+        let mut runtime = WasmtimeRuntime::new();
+        let module_path = format!("{}{}", TEST_PATH, module_name);
+        let config = ModuleConfig::new(
+            "aaaa-bbbbb-ccccc".to_string(),
+            "invert_colors.wasm".to_string(),
+            module_path.clone(),
+            data_files.clone(),
+            None,
+        );
+        let result = runtime.load_module(config);
+        match result {
+            Ok(_) => {
+                println!("✅ Successfully loaded: {}", module_name);
+            }
+            Err(e) => {
+                println!("❌ Failed to load {}: {}", module_name, e);
+                assert!(false, "");
+            }
+        }
+        if let Some(module) = runtime.modules.get("invert_colors.wasm") {
+            let funcs = module.get_all_functions();
+            println!("Module {} has following function imports: {:?}", module.name, funcs);
+            Ok(())
+        } else {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_stateful_fibo() -> Result<(), Box<dyn std::error::Error>> {
+        let f = |_: &std::panic::PanicHookInfo| {};
+        std::panic::set_hook(Box::new(f));
+        let module_name = "stateful_fibo.wasm";
+        let data_files: HashMap<String, String> = HashMap::new();
+        let mut runtime = WasmtimeRuntime::new();
+        let module_path = format!("{}{}", TEST_PATH, module_name);
+        let config = ModuleConfig::new(
+            "aaaa-bbbbb-ccccc".to_string(),
+            "stateful_fibo.wasm".to_string(),
+            module_path.clone(),
+            data_files.clone(),
+            None,
+        );
+        let result = runtime.load_module(config);
+        match result {
+            Ok(_) => {
+                println!("✅ Successfully loaded: {}", module_name);
+            }
+            Err(e) => {
+                println!("❌ Failed to load {}: {}", module_name, e);
+                assert!(false, "");
+            }
+        }
+        if let Some(module) = runtime.modules.get("stateful_fibo.wasm") {
+            let funcs = module.get_all_functions();
+            println!("Module {} has following function imports: {:?}", module.name, funcs);
+            Ok(())
+        } else {
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn test_wasi_mobilenet_onnx() -> Result<(), Box<dyn std::error::Error>> {
+        let f = |_: &std::panic::PanicHookInfo| {};
+        std::panic::set_hook(Box::new(f));
+        let module_name = "wasi_mobilenet_onnx.wasm";
+        let data_files: HashMap<String, String> = HashMap::new();
+        let mut runtime = WasmtimeRuntime::new();
+        let module_path = format!("{}{}", TEST_PATH, module_name);
+        let config = ModuleConfig::new(
+            "aaaa-bbbbb-ccccc".to_string(),
+            "wasi_mobilenet_onnx.wasm".to_string(),
+            module_path.clone(),
+            data_files.clone(),
+            None,
+        );
+        let result = runtime.load_module(config);
+        match result {
+            Ok(_) => {
+                println!("✅ Successfully loaded: {}", module_name);
+            }
+            Err(e) => {
+                println!("❌ Failed to load {}: {}", module_name, e);
+                assert!(false, "");
+            }
+        }
+        if let Some(module) = runtime.modules.get("wasi_mobilenet_onnx.wasm") {
+            let funcs = module.get_all_functions();
+            println!("Module {} has following function imports: {:?}", module.name, funcs);
+            Ok(())
+        } else {
+            Ok(())
+        }
     }
 
 }
